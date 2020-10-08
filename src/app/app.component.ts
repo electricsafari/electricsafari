ff6185c3ae8aed4a39a72f150107b9c66efb70c6
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2020-10-08 22:50:41 +0100

    ELSAF-8: Display twitter link on landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elsaf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit(): void {
-    console.log('What are you looking for? We\'ve only got a single <h1> on the screen!');
-  }
+export class AppComponent {
 }
